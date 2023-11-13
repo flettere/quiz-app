59122b4fc6f4f8fd4ae96fb34115e929a43f6b0e
federico.lettere <federico.lettere@mermecgroup.com>
2023-11-13 14:17:05 +0100

    added http module

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,7 @@ import { AppComponent } from './app/app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { provideRouter } from '@angular/router';
 import { routes } from './app/app.routes';
+import { HttpClientModule } from '@angular/common/http';
 
 if (environment.production) {
   enableProdMode();
@@ -15,6 +16,7 @@ if (environment.production) {
 bootstrapApplication(AppComponent, {
   providers: [
     importProvidersFrom(BrowserAnimationsModule),
+    importProvidersFrom(HttpClientModule),
     provideRouter(routes),
   ],
 });
